49285ae3005a87ce4f8328bc3b1f45a5d22a6b00
miawpuspita <puspitasari22232@gmail.com>
2024-05-02 15:13:55 +0700

    fakultasSI4B

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
+// jika tidak ada maka muncul error
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +16,25 @@ route::get('about',function(){
 route::get('profil',function(){
     return view('profil');
 });
+
+// route dengan parameter
+Route::get('welcome/{salam}', function ($salam) {
+    // return 'selamat ' . $salam;
+    return view('salam')->with('viewsalam',$salam);
+});
+
+// route tanpa parameter listdata
+// terdapat array list
+Route::get('listdata',function() {
+    $list = ["sistem informasi","informatika","manajemen"];
+    $listmhs= [
+        ["npm"=> 001, "nama" => "ahmad"],
+        ["npm"=> 002, "nama" => "budi"]
+    ];
+    return view('listprodi')
+    -> with ('viewlist',$list)
+    -> with ('viewmhs',$listmhs);
+});
+
+route::resource('fakultas',
+FakultasController::class);
