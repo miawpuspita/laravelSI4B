5af51acd004525099203152a00ecf56855b9efd0
puspitasari <puspitasari22232@gmail.com>
2024-05-21 17:01:13 +0700

    materi terbaru

--- example-app/resources/views/prodi/index.blade.php
+++ example-app/resources/views/prodi/index.blade.php
@@ -1,19 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout.main')
+
+@section('title', 'Prodi')
+
+@section('content')
     <h1>UMDP</h1>
-    <h2>program studi</h2>
-    <ul>
-        @foreach ($prodi as $item)
-    <li>{{ $item["nama"] }} {{ $item["singkatan"] }}
-    {{ $item["fakultas"] ["nama"] }}  </li>
-    @endforeach
-</ul>
-</body>
-</html>
+    <h2>Prodi</h2>
+    <div class="col-lg-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Prodi</h4>
+                  <p class="card-description">
+                    List Data Prodi
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Nama Prodi</th>
+                          <th>Singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                         @foreach ($prodi as $value)
+                            <tr>
+                                <td>{{ $value["nama"] }}</td>
+                                <td>{{ $value["singkatan"] }} </td>
+                            </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+@endsection
